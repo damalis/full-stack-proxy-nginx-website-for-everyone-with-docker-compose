3aadfab59ff947abf7d037119b313ec4e5cbc9b3
Erdal ALTIN <erdalaltin@gmail.com>
2022-06-06 10:42:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 clear
 echo ""
-echo "================================================================="
-echo "|                                                               |"
-echo "|     full-stack-webserver-for-everyone-with-docker-compose     |"
-echo "|                       by Erdal ALTIN                          |"
-echo "|                                                               |"
-echo "================================================================="
+echo "======================================================================="
+echo "|                                                                     |"
+echo "|  full-stack-proxy-nginx-webserver-for-everyone-with-docker-compose  |"
+echo "|                          by Erdal ALTIN                             |"
+echo "|                                                                     |"
+echo "======================================================================="
 sleep 2
 
 # Uninstall old versions
