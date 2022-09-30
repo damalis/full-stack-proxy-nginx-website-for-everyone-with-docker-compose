c6251558e6d539db4499282f554390186dd1f7c7
Erdal ALTIN <erdalaltin@gmail.com>
2022-09-30 12:26:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ clear
 echo ""
 echo "======================================================================="
 echo "|                                                                     |"
-echo "|  full-stack-proxy-nginx-webserver-for-everyone-with-docker-compose  |"
+echo "|   full-stack-proxy-nginx-website-for-everyone-with-docker-compose   |"
 echo "|                          by Erdal ALTIN                             |"
 echo "|                                                                     |"
 echo "======================================================================="
